1966143dcfae9e368a0a4084fce136643ddb778b
Sauldejesuscortez <al221710926@gmail.com>
2021-02-18 01:27:45 -0600

    07 Requesting a token

--- api/app/Http/Controllers/Auth/TokenController.php
+++ api/app/Http/Controllers/Auth/TokenController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationExceptionn;
 
 class TokenController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-      //
+      $this->validate($request, [
+          'email' => 'required|email',
+          'password' => 'required',
+
+
+      ]);
+
+      if (!auth()->attempt($request->only('email', 'password'))){
+          throw new AuthenticationException();
+         }
+
+         return [
+             'token' => auth()->user()->createToken('test')->plainTextToken
+         ];
 
     }
 }
