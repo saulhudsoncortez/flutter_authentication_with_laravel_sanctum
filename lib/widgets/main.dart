24cced0b8caf5b480cc63f9d67e6b6c36a46990c
Sauldejesuscortez <saul32498@gmail.com>
2021-04-17 16:58:05 -0500

    11 Storing the token securely

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -76,6 +76,84 @@ class _MyHomePageState extends State<MyHomePage> {
        // This trailing comma makes auto-formatting nicer for build methods.
     );
     drawer: NavDrawer(),
+      body: Center(import 'package:flutter/material.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/providers/auth.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/widgets/nav-drawer.dart';
+import 'package:provider/provider.dart';
+
+
+
+
+void main()   {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => Auth(),
+      child: MyApp(),
+
+    )
+  );
+ 
+  
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+        
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Sanctum authentication'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: NavDrawer(),
+      body: Center(
+        child: Consumer<Auth>(
+          builder: (context, auth, child){
+            if (auth.authenticated){
+              return Text('You are logged in');
+            }else{
+              return Text('You are not logged in');
+            }
+          }
+        )
+
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+    drawer: NavDrawer(),
       body: Center(
         child: Consumer<Auth>(
           builder: (context, auth, child){
@@ -92,6 +170,43 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: NavDrawer(),
+      body: Center(
+        child: Consumer<Auth>(
+          builder: (context, auth, child){
+            if (auth.authenticated){
+              return Text('You are logged in');
+            }else{
+              return Text('You are not logged in');
+            }
+          }
+        )
+
+      ),
+        child: Consumer<Auth>(
+          builder: (context, auth, child){
+            if (auth.authenticated){
+              return Text('You are logged in');
+            }else{
+              return Text('You are not logged in');
+            }
+          }
+        )
+
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
 
     return Scaffold(
       appBar: AppBar(
